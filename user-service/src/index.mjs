6965ecef4afa47902efa8c79ec1c5cd10faf57f8
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2024-12-06 17:45:27 +0100

    daily

--- user-service/src/index.mjs
+++ user-service/src/index.mjs
@@ -2,10 +2,23 @@ import express from 'express'
 const app = express()
 const PORT = 80
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/users/add', (req, res) => {
+  throw new Error("Not implemented")
+  
+})
+
+app.get('/users/getToken', (req, res) => {
+  throw new Error("Not implemented")
+})
+
+app.post('/users/checkToken', (req, res) => {
+  throw new Error("Not implemented")
+})
+
+app.get('/users/:id', (res, req) => {
+  throw new Error("Not implemented")
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Service has been started. Listening on port ${PORT}`)
 })
