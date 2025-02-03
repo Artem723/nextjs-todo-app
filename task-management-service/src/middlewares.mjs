41669cb812bd3af3d6da6b0ddbab1450e877f40b
Artiom Savchuk <artemsavchuk01@gmail.com>
2025-02-03 23:27:52 +0100

    implemented middleware for taking user ID

--- task-management-service/src/middlewares.mjs
+++ task-management-service/src/middlewares.mjs
@@ -1,7 +1,20 @@
 import logger from "./logger/index.mjs";
 
 export function placeUserData(req, res, next) {
-    logger.info('Middleware executed');
-    throw new Error("Not implemented!");
+    const token = req.cookies?.token;
+    console.log('token: ', token)
+    if (!token) {
+        res.status(401).end('Unauthorized.');
+        return;
+    }
+    let payload = null;
+    try {
+        payload = JSON.parse(atob(token.split('.')[0]));
+    } catch(e) {
+        logger.error(e);
+        logger.error(`Cannot parse the JWT payload: ${token.split('.')[0]}`)
+        return;
+    }
+    res.locals.userID = payload.id;
     next();
 }
