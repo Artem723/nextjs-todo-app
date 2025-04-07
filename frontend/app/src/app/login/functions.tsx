24d332369f023548f1807de23ad558635d576f31
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2025-04-07 16:41:56 +0200

    updated networking in docker compose

--- frontend/app/src/app/login/functions.tsx
+++ frontend/app/src/app/login/functions.tsx
@@ -12,14 +12,14 @@ export interface StateObj {
 }
 export async function loginSubmit(state: StateObj | null, formData: FormData): Promise<StateObj | null> {
     const login = formData.get('login');
-    const pwd = formData.get("password");
+    const password = formData.get("password");
     
     const request = {
         method: "POST",
         headers: {
             'Content-Type': "application/json"
         },
-        body: JSON.stringify({}),
+        body: JSON.stringify({login, password}),
 
     }
     try {
